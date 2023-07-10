dcb24a13b3df16807113bdc43b7901e2b7d1a608
franfreezy <64470491+franfreezy@users.noreply.github.com>
2023-07-11 00:03:10 +0300

    Add files via upload
    third commit

--- index.php
+++ index.php
@@ -6,25 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- CSS Links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="admin-assets/styles.css" />
+    <link rel="stylesheet" href="assets/styles.css" />
     <!-- Animate Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
-    <title>Admin | Savannah Agile Solutions</title>
-    <link rel="icon" type="image/x-icon" href="../images/favicon.png"/>
+    <title>Savannah Agile Solutions</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.png"/>
   </head>
 <body>
+    <?php  
+    require('connect.php'); 
+    ?>
     <!-- Company Name & Logo -->
     <div class="main-section">
         <div class="hero-section"></div>
         <div class="user-sign-in">
             <!-- Company Logo -->
             <div class="company-logo scale-in-center">
-                <img src="../images/logo-symbol.png" alt="">
+                <img src="images/logo-symbol.png" alt="">
             </div>
             <div class="logo-img scale-in-center">                
-                <img src="../images/Logo_SavannahAgile-1.png" alt="Logo Image">
+                <img src="images/Logo_SavannahAgile-1.png" alt="Logo Image">
             </div>
             <!-- Sign in Form -->
             <div class="log-in-form">
@@ -60,22 +63,24 @@
             </div>
         </div>
     </div>
+
+
     <!-- Js Links -->
     <!-- Password Toggle -->
-    <script>   
-    const togglePassword = document.querySelector('#show-password');
-    const password = document.querySelector('#password');
+<script>   
+const togglePassword = document.querySelector('#show-password');
+const password = document.querySelector('#password');
     togglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
     // toggle the eye slash icon
     this.classList.toggle('.fa-eye');
-    });
-    </script>
+});
+</script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
-    <script src="admin-assets/script.js"></script>
+    <script src="assets/script.js"></script>
 </body>
 </html>
